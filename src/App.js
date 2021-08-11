b14d5a85469dac14bd48bc50ea601e9ea1b9a895
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-08-11 16:45:09 -0300

    Data fetching

--- src/App.js
+++ src/App.js
@@ -1,39 +1,50 @@
 import { Component } from 'react';
 
+import './App.css';
+
 class App extends Component {
   state = {
-    posts: [
-      {
-        id: 1,
-        title: 'O título 1',
-        body: 'O corpo 1',
-      },
-      {
-        id: 2,
-        title: 'O título 2',
-        body: 'O corpo 2',
-      },
-      {
-        id: 3,
-        title: 'O título 3',
-        body: 'O corpo 3',
-      },
-    ]
+    posts: []
+  }
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+    
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url  }
+    });
+
+    this.setState({ posts: postsAndPhotos });
   }
 
   render() {
     const { posts } = this.state;
 
     return (
-      <div className="App">
-        {posts.map(post => (
-          <div key={post.id}>
-            <h1>{post.title}</h1>
-            <p>{post.body}</p>
-          </div>
-          
-        ))}
-      </div>
+      <section className="container">
+        <div className="posts">
+          {posts.map(post => (
+            <div className="post" alt={post.title}>
+              <img src={post.cover} />
+              <div key={post.id} className="post-content">
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+      
     )
   }
 }
