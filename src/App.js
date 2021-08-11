8ac775183e98211c25f9e43301b87b258f88dd22
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-08-11 15:19:02 -0300

    State com arrays e objetos

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { Component } from 'react';
+
+class App extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: 'O título 1',
+        body: 'O corpo 1',
+      },
+      {
+        id: 2,
+        title: 'O título 2',
+        body: 'O corpo 2',
+      },
+      {
+        id: 3,
+        title: 'O título 3',
+        body: 'O corpo 3',
+      },
+    ]
+  }
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <div className="App">
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+          
+        ))}
+      </div>
+    )
+  }
+}
+
+export default App;
